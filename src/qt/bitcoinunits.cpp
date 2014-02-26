1b36bf14fedf4d7ff23744674317c143e9832fc2
Page <page@ubuntu.(none)>
2014-02-26 09:52:30 -0800

    changes

--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -34,9 +34,9 @@ QString BitcoinUnits::name(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("SMC");
-    case mBTC: return QString("mSMC");
-    case uBTC: return QString::fromUtf8("μSMC");
+    case BTC: return QString("NCC");
+    case mBTC: return QString("mNCC");
+    case uBTC: return QString::fromUtf8("μNCC");
     default: return QString("???");
     }
 }
@@ -45,9 +45,9 @@ QString BitcoinUnits::description(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("SmallChange");
-    case mBTC: return QString("Milli-SmallChange (1 / 1,000)");
-    case uBTC: return QString("Micro-SmallChange (1 / 1,000,000)");
+    case BTC: return QString("newcenturycoin");
+    case mBTC: return QString("Milli-newcenturycoin (1 / 1,000)");
+    case uBTC: return QString("Micro-newcenturycoin (1 / 1,000,000)");
     default: return QString("???");
     }
 }
